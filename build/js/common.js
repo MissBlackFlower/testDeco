297a1d6e651a48ff9f0d6e575ecdd4f8497d67b7
Viktoriia <Babenko_v_v@ukr.net>
2016-11-21 10:50:25 +0200

    upd

--- build/js/common.js
+++ build/js/common.js
@@ -1,5 +1,36 @@
 $(document).ready(function() {
 
-	// go js
+	$("#js_nav").on("click","a", function (event) {
+			 event.preventDefault();
 
+			 var id  = $(this).attr('href'),
+					 top = $(id).offset().top;
+
+			 $('body,html').animate({scrollTop: top}, 1500);
+	 });
+
+		//  показать моделку по клику на ссылку
+	 $(".js_service").on("click", function (event) {
+				var wrap = $(this).closest(".js-col"),
+						modal = wrap.find(".js_modal");
+
+						wrap.addClass('is-active');
+						modal.slideDown();
+				event.preventDefault();
+		});
+		// скрыть модалку по клику на любой её части
+		$(".js_modal").on("click", function (event) {
+				 var wrap = $(this).closest(".js-col");
+
+						 wrap.removeClass('is-active');
+						 $(this).slideUp();
+				 event.preventDefault();
+		 });
+
+		 $(".js_btn-subject").on("click", function (event) {
+					var list = $(".js_subject-list");
+
+							$(this).toggleClass('is-show');
+							list.slideToggle();
+			});
 });
