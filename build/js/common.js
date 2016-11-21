9a05aec24b851bf544f0c1cc99be07376681fab9
Viktoriia <Babenko_v_v@ukr.net>
2016-11-21 15:31:51 +0200

    upd

--- build/js/common.js
+++ build/js/common.js
@@ -9,28 +9,40 @@ $(document).ready(function() {
 			 $('body,html').animate({scrollTop: top}, 1500);
 	 });
 
-		//  показать моделку по клику на ссылку
+	 $(".js_to-prise").on("click", function () {
+				var top = $("#price").offset().top;
+
+				$('body,html').animate({scrollTop: top}, 1500);
+		});
+
+	 $(".js_to-order").on("click", function () {
+				var top = $("#contacts").offset().top;
+						modal = $(this).closest(".js_modal"),
+						wrap = modal.closest(".js_col");
+
+				setTimeout(
+					"wrap.removeClass('is-active');	modal.slideUp();",
+					1000
+				);
+
+				$('body,html').animate({scrollTop: top}, 1500);
+		});
+
+		//  показать/скрыть модалку по клику на ссылку
 	 $(".js_service").on("click", function (event) {
-				var wrap = $(this).closest(".js-col"),
+				var wrap = $(this).closest(".js_col"),
 						modal = wrap.find(".js_modal");
 
-						wrap.addClass('is-active');
-						modal.slideDown();
+				wrap.toggleClass('is-active');
+				modal.slideToggle();
 				event.preventDefault();
 		});
-		// скрыть модалку по клику на любой её части
-		$(".js_modal").on("click", function (event) {
-				 var wrap = $(this).closest(".js-col");
 
-						 wrap.removeClass('is-active');
-						 $(this).slideUp();
-				 event.preventDefault();
-		 });
-
+			//  показать/скрыть список услуг по клику на кнопку
 		 $(".js_btn-subject").on("click", function (event) {
 					var list = $(".js_subject-list");
 
-							$(this).toggleClass('is-show');
-							list.slideToggle();
+					$(this).toggleClass('is-show');
+					list.slideToggle();
 			});
 });
